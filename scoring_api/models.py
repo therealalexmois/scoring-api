102901e07d5efca1a53c10888dc90792145274e9
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2025-03-07 14:54:28 +0300

    feat: add HTTPResponse model

--- scoring_api/models.py
+++ scoring_api/models.py
@@ -3,10 +3,26 @@
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from scoring_api.constants import HTTPStatus
+
 if TYPE_CHECKING:
     from typing import Any
 
-    from scoring_api.constants import HTTPStatus
+
+@dataclass
+class HTTPResponse:
+    """Представляет собой стандартизированный ответ HTTP об успехе."""
+
+    response: dict[str, 'Any']
+    status: HTTPStatus = HTTPStatus.OK
+
+    def as_tuple(self) -> tuple[dict[str, 'Any'], int]:
+        """Возвращает ответ в виде кортежа с целочисленным кодом состояния."""
+        return {'response': self.response}, self.status.value
+
+    def __getitem__(self, index: int) -> dict[str, 'Any'] | int:
+        """Разрешить распаковку как кортежа."""
+        return self.as_tuple()[index]
 
 
 @dataclass
@@ -20,6 +36,6 @@ class HTTPErrorResponse:
         """Возвращает ошибку в виде кортежа."""
         return {'error': self.error}, self.status.value
 
-    def __getitem__(self, index: int) -> 'Any':
+    def __getitem__(self, index: int) -> dict[str, 'Any'] | int:
         """Allow unpacking like a tuple."""
         return self.as_tuple()[index]
