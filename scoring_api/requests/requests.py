bf5c93361ed7d082c32f8728c8f4fe5c469fd90d
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2025-03-10 22:08:34 +0300

    feat: add additional convert str to datetime

--- scoring_api/requests/requests.py
+++ scoring_api/requests/requests.py
@@ -1,6 +1,7 @@
 # ruff: noqa: D102, D107, ANN401
 """Проверка и обработка запросов."""
 
+from datetime import datetime
 from typing import TYPE_CHECKING
 
 from scoring_api.constants import ADMIN_LOGIN
@@ -36,6 +37,21 @@ class OnlineScoreRequest(BaseRequest):
     phone: 'ClassVar[PhoneField]' = PhoneField(required=False, nullable=True)
     birthday: 'ClassVar[BirthDayField]' = BirthDayField(required=False, nullable=True)
     gender: 'ClassVar[GenderField]' = GenderField(required=False, nullable=True)
+
+    def is_valid(self) -> bool:
+        """Дополнительная валидация после основного метода."""
+        valid = super().is_valid()
+
+        if valid and 'birthday' in self.validated_data and isinstance(self.validated_data['birthday'], str):
+            try:
+                self.validated_data['birthday'] = datetime.strptime(
+                    self.validated_data['birthday'], '%d.%m.%Y'
+                ).date()
+            except ValueError:
+                self.errors['birthday'] = 'Invalid date format. Expected DD.MM.YYYY'
+                return False
+
+        return valid
 
 
 class MethodRequest(BaseRequest):
